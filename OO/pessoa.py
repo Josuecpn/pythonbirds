9992c9822f57ce9cbdfd61bb2280181d0de38f16
josuecpn <josuecpn93@gmail.com>
2020-08-23 15:00:52 -0300

    Criado atributo complexo 'filhos'

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,19 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=int(0)):
+    def __init__(self, *filhos, nome=None, idade=0):
         # None deixa a variável default sem valor, permitindo que instancie
         # com atributo sem valor
         self.nome = nome
-        self.idade = idade
+        self.idade = int(idade)
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return 'Olá'
+        return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa(nome='Pedro',idade=25)
-    p2 = Pessoa('João', 27)
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
-    print(p2.cumprimentar())
-    print(p2.nome)
-    print(p2.idade)
+    joao = Pessoa(nome='João', idade=17)
+    pedro = Pessoa(joao, nome='Pedro', idade=45)
+    print(pedro.cumprimentar())
+    print(pedro.nome)
+    print(pedro.idade)
+    for filho in pedro.filhos:
+        print(filho.nome)
+    print(joao.cumprimentar())
+    print(joao.nome)
+    print(joao.idade)
