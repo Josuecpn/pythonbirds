31798ba94cbc7925d953a6837f410b56078eb09a
josuecpn <josuecpn93@gmail.com>
2020-08-23 14:16:55 -0300

    add atributos de instancia nome e idade

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,7 +1,19 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=int(0)):
+        # None deixa a variável default sem valor, permitindo que instancie
+        # com atributo sem valor
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa(nome='Pedro',idade=25)
+    p2 = Pessoa('João', 27)
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
+    print(p2.cumprimentar())
+    print(p2.nome)
+    print(p2.idade)
